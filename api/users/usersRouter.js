e77a2b0bed31eb629c0928b065b5d4b0e2732eca
Kevin Lopez <kevin_lopez@live.com>
2021-03-10 13:22:42 -0600

    makes login endpoint

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const Users = require('./usersModel')
+const jwt = require('jsonwebtoken')
 
 const router = express.Router()
 
 // register new user
 router.post('/register', async (req, res) => {
-    console.log('sanity check')
     const { email, password } = req.body
 
     if(email && password) {
@@ -21,5 +21,44 @@ router.post('/register', async (req, res) => {
         res.status(400).json({error: 'Must include email and password'})
     }
 })
+
+// login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    if(email && password) {
+        const user = await Users.getByEmail(email)
+
+        if(await bcrypt.compare(password, user.password)) {
+            const token = generateToken(user)
+            res.status(200).json({user: user.email, token})
+        } else {
+            res.status(401).json({error: 'Invalid email or password'})
+        }
+
+    } else {if(await bcrypt.compare())
+        res.status(400).json({error: 'Must include email and password'})
+    }
+})
+
+// test auth
+router.get('/', (req, res) => {
+    const { email, password, token } = req.body
+
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+    res.send(decoded)
+})
+
+function generateToken(user) {
+    const payload = {
+      user: user.id,
+    }
+  
+    const options = {
+      expiresIn: '1d',
+    }
+
+    return jwt.sign(payload, process.env.TOKEN_SECRET, options)
+}
 
 module.exports = router
