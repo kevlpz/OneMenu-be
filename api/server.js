438c98ac2455c665c2192285a43b2681b88983c1
Kevin Lopez <kevin_lopez@live.com>
2021-03-22 20:29:06 -0500

    .

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const categoriesRouter = require('./categories/categoriesRouter')
 const dishesRouter = require('./dishes/dishesRouter')
 
 // server.use(cors({
-//     origin: 'https://wsky-fe.herokuapp.com',
+//     origin: 'http://localhost:5000',
 //     credentials: true
 // }))
 server.use(helmet())
